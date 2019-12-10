c8291ff0ed1d25d7ce7b86a57ea4c8b6a67474fe
code-sparrow <code.sparrow7@gmail.com>
2019-12-09 19:37:32 -0500

    finished all 8 querries, and bonus notebook

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -54,7 +54,7 @@ JOIN departments d ON de.dept_no = d.dept_no;
 last names begin with "B."
 */
 
-SELECT * FROM employees
+SELECT first_name, last_name FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
@@ -65,14 +65,51 @@ AND last_name LIKE 'B%';
 their employee number, last name, first name, and department name.
 */
 
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+
 /*
 -------------------------------------------------------------------
 7. List all employees in the Sales and Development departments,
 including their employee number, last name, first name, and department name.
 */
 
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+OR d.dept_name = 'Development';
+
+
 /*
 -------------------------------------------------------------------
 8. In descending order, list the frequency count of employee last names, 
 i.e., how many employees share each last name.
 */
+
+SELECT last_name, COUNT(last_name) AS frequency_count
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;
+
+/*
+Epilogue
+Evidence in hand, you march into your boss's office and present
+the visualization. With a sly grin, your boss thanks you for your work.
+On your way out of the office, you hear the words, "Search your ID number." 
+You look down at your badge to see that your employee ID number is 499942.
+*/
+
+SELECT * FROM employees
+WHERE emp_no = 499942;
+--April Foolsday in December?
+--I've been tricked
+--Boss is jerk
+--I feel dirty and used
+--I'm going to bed...
